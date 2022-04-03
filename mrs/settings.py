4725179110d5cef2fcdbccad88e46bcefbb1bf7a
Utsav <utsavkhadgi8@gmail.com>
2022-04-03 14:25:15 +0545

    removed errors

--- mrs/settings.py
+++ mrs/settings.py
@@ -129,7 +129,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
