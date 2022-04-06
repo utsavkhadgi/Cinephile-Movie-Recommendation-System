b6d5f90db396117279b562ade1c32f375c27e2b5
Utsav <utsavkhadgi8@gmail.com>
2022-04-06 06:58:30 +0545

    final touches

--- mrs/settings.py
+++ mrs/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k^+6*tf(&em26lt2sgc%y1l(z=5l=omhqd39x=jti=^86i2$9v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cinephile-movie-library.herokuapp.com','localhost']
 
@@ -129,7 +129,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 
